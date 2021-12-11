7c9e2a51152e1787a8ce7354b6cea25d032d9013
lukemunsch <lpmunsch@gmail.com>
2021-12-11 16:19:43 +0000

    add content to js and test files

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,1 +1,25 @@
-const addition = require("../calc")
+const {
+    TestWatcher
+} = require("jest");
+
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("addition test", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 41 + 32", () => {
+            expect(addition(41, 32)).toBe(73);
+        });
+    });
+    describe("subtract test", () => {
+
+    });
+    describe("multiply test", () => {
+
+    });
+    describe("divide test", () => {
+
+    });
+});
